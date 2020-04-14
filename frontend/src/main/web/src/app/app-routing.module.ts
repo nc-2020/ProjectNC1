dda48a0adbdaa64a3b98a73a9cdf399a20dd104d
Tornado9966 <dzhanoyants9966@gmail.com>
2020-04-14 12:22:21 +0300

    Add navigation between authorization and registration

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import {AuthorizationComponent} from "./authorization/authorization.component";
 import {HomeComponent} from "./home/home.component";
 import {AppComponent} from "./app.component";
+import {RegistrationComponent} from "./registration/registration.component";
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login'},
   // { path: '', component: AppComponent},
   { path: 'home', component: HomeComponent},
-  { path: 'login', component: AuthorizationComponent}
+  { path: 'login', component: AuthorizationComponent},
+  { path: 'signup', component: RegistrationComponent }
 ];
 
 @NgModule({
