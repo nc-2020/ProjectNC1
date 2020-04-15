2108ada564679606673ab82d9df858095d007fb3
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2020-04-15 21:03:52 +0300

    Modify user DTOs

---
+++ backend/src/main/java/com/team/app/backend/dto/UserLoginDto.java
@@ -0,0 +1,25 @@
+package com.team.app.backend.dto;
+
+public class UserLoginDto {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserLoginDto setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserLoginDto setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
