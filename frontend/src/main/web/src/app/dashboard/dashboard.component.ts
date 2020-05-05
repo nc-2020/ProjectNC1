7df38f1558148110e75a54cc03f1342fce9ca3ca
Tornado9966 <dzhanoyants9966@gmail.com>
2020-05-05 12:29:13 +0300

    Search update

--- frontend/src/main/web/src/app/dashboard/dashboard.component.ts
+++ frontend/src/main/web/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit, Input, ViewChild, AfterViewChecked, AfterViewInit} from '@angular/core';
 import {UserService} from "../user.service";
+import { QuizService } from "../services/quiz.service";
 import { User } from '../entities/user';
+import { Quiz } from '../entities/quiz';
 import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import {Location} from '@angular/common';
 import { SharedUserDataService } from '../shared-user-data.service';
 import {UserCardComponent} from "../user-card/user-card.component";
+import {QuizCardComponent} from "../quiz-card/quiz-card.component";
 import {UserProfileComponent} from "../user-profile/user-profile.component";
 import * as bcrypt from 'bcryptjs';
 import {CategoryService} from "../services/category.service";
@@ -22,28 +25,31 @@ export class DashboardComponent implements OnInit {
   tab: string = '';
   user: User;
   users$: Observable<User[]>;
+  quizes$: Observable<Quiz[]>;
   categoriesList: Category[] = [];
+  isVisible = true;
 
 
   private searchTerms = new Subject<string>();
   @ViewChild(UserCardComponent, {static: false}) userCardChild: UserCardComponent;
 
-  constructor(private userService: UserService, private categoryService: CategoryService, private location: Location, private route: ActivatedRoute,
+  constructor(private userService: UserService, private quizService: QuizService, private categoryService: CategoryService, private location: Location, private route: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit(): void {
     this.getCategories();
     this.tab = this.route.snapshot.paramMap.get('tab');
     this.user = this.tab === 'Profile' ? this.userService.user : {role:{}} as User;
-    this.users$ = this.searchTerms.pipe(
+    this.quizes$ = this.searchTerms.pipe(
       debounceTime(300),
       distinctUntilChanged(),
       // switch to new search observable each time the term changes
-      switchMap((term: string) => this.userService.searchUsers(term)),
+      switchMap((term: string) => this.quizService.searchQuizzes(term)),
     );
   }
 
   search(term: string): void {
+    this.isVisible = false;
     this.searchTerms.next(term);
   }
   profileSet( editOnly?: boolean, user?: User)
@@ -55,6 +61,7 @@ export class DashboardComponent implements OnInit {
   changeTab(tab: string) {
     this.tab = tab;
     this.router.navigateByUrl(`dashboard/${tab}`);
+    this.isVisible = true;
   }
 
   getUser() {
