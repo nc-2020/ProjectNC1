43aa99321eefddccdbd2063de53cebb8df76b6db
Demon3003 <zhuravlyovdima34@gmail.com>
2020-05-10 23:14:19 +0300

    fix bug

--- frontend/src/main/web/src/app/services/question.service.ts
+++ frontend/src/main/web/src/app/services/question.service.ts
@@ -10,7 +10,7 @@ import {UserService} from "./user.service";
 })
 export class QuestionService {
 
-  private questionsUrl = 'http://localhost:8080/api/question';  // URL to web api
+  private questionsUrl = 'http://localhost:8080/api/question/';  // URL to web api
 
   constructor(private http: HttpClient, private userService: UserService) {
   }
@@ -26,14 +26,14 @@ export class QuestionService {
   }
 
   getQuestion(question_id): Observable<Question> {
-    return  this.http.get<Question>(this.questionsUrl + '/question/' + question_id, { headers: new HttpHeaders()
+    return  this.http.get<Question>(this.questionsUrl + question_id, { headers: new HttpHeaders()
         .set('Authorization',  `Bearer_${this.userService.getToken()}`)})
   }
 
   /** POST: add a new question to the server */
   createQuestion(question: Question): Observable<Question> {
     console.table(question);
-    return this.http.post<Question>(this.questionsUrl + '/question/' + question.type.id, question, { headers: new HttpHeaders()
+    return this.http.post<Question>(this.questionsUrl  + question.type.id, question, { headers: new HttpHeaders()
         .set('Authorization',  `Bearer_${this.userService.getToken()}`)}).pipe(
       catchError(this.handleError<Question>('createQuestion'))
     );
