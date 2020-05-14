c21037cf9e555e91d6bfe1625acd4c5cc74c5d25
Zung Nguen <38420456+kanzert@users.noreply.github.com>
2020-05-14 19:45:59 +0300

    add sql to external file

--- backend/src/main/java/com/team/app/backend/persistance/dao/impl/AchievementDaoImpl.java
+++ backend/src/main/java/com/team/app/backend/persistance/dao/impl/AchievementDaoImpl.java
@@ -4,15 +4,29 @@ import com.team.app.backend.persistance.dao.AchievementDao;
 import com.team.app.backend.persistance.dao.mappers.AchievementRowMapper;
 import com.team.app.backend.persistance.model.Achievement;
 import com.team.app.backend.persistance.model.UserAchievement;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+
 
 import javax.sql.DataSource;
 import java.util.List;
 
+
+
+
+
+@PropertySource("classpath:sql_query.properties")
 @Repository
 public class AchievementDaoImpl implements AchievementDao {
     private final JdbcTemplate jdbcTemplate;
+    @Value("${achievement.all}")
+    private String sqlGetAchievements;
+    @Value("${achievement.user}")
+    private String sqlGetUserAchievements;
+
 
     private AchievementRowMapper achievementRowMapper = new AchievementRowMapper();
 
@@ -22,15 +36,8 @@ public class AchievementDaoImpl implements AchievementDao {
 
     @Override
     public List<UserAchievement> getUserAchievements(long userId) {
-        return jdbcTemplate.query("SELECT A.title, A.quiz_amount, UA.played " +
-                        "FROM achievement A " +
-                        "LEFT OUTER JOIN (SELECT A.title, COUNT(QTC.cat_id) AS played " +
-                        "FROM user_to_ses US " +
-                        "INNER JOIN session S ON US.ses_id = S.id " +
-                        "INNER JOIN quiz_to_categ QTC ON QTC.quiz_id = S.quiz_id " +
-                        "INNER JOIN achievement A ON A.cat_id = QTC.cat_id " +
-                        "WHERE US.user_id = ? " +
-                        "GROUP BY QTC.cat_id, A.title) AS UA ON A.title = UA.title", new Object[] { userId },
+        System.out.println(sqlGetUserAchievements);
+        return jdbcTemplate.query(sqlGetUserAchievements, new Object[] { userId },
                 (resultSet, i) -> {
                     UserAchievement userAchievement = new UserAchievement();
                     userAchievement.setTitle(resultSet.getString("title"));
@@ -42,6 +49,6 @@ public class AchievementDaoImpl implements AchievementDao {
 
     @Override
     public List<Achievement> getAchievements() {
-        return jdbcTemplate.query("SELECT * FROM achievement", achievementRowMapper);
+        return jdbcTemplate.query(sqlGetAchievements, achievementRowMapper);
     }
 }
